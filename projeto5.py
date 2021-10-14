383f50701a30826632c5aa33b0e230b6fcf49cf7
Pchenrique <pchenrique1998@hotmail.com>
2021-10-14 00:28:07 -0300

    codeless improvement

--- projeto5.py
+++ projeto5.py
@@ -9,8 +9,8 @@ dados = np.genfromtxt("colorrectal_2_classes_formatted.txt", delimiter=",")
 # Getting classes
 classes = dados[:, 0:142]
 attributes = dados
+
 # Getting attributes and normalizing
-#attributes = np.delete(dados,(142), axis=1)
 min_max_scaler = MinMaxScaler()
 attributes_norm = min_max_scaler.fit_transform(attributes)
 
@@ -20,7 +20,7 @@ kmeans.fit(attributes_norm)
 
 labels = kmeans.labels_
 
-plt.scatter(attributes_norm[:, 0], attributes_norm[:,1], s = 100, c = kmeans.labels_)
+plt.scatter(attributes_norm[:, 10], attributes_norm[:,15], s = 100, c = kmeans.labels_)
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'red',label = 'Centroids')
 plt.title('Colorrectal Clusters and Centroids')
 plt.xlabel('SepalLength')
